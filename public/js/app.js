bc63743564e72768896e831bcf17c81e9cae541f
Stefan Bohacek <stefan@stefanbohacek.com>
2024-03-14 15:12:08 -0400

    Removed unused code.

--- public/js/app.js
+++ public/js/app.js
@@ -49,32 +49,26 @@ onReady(() => {
     editor.Panels.getButton("views", "open-blocks").set("active", true);
   });
 
-
-
-  const bm = editor.Blocks;
-
-
-  const imageBlock = bm.get('image');
+  // const bm = editor.Blocks;
+  // const imageBlock = bm.get('image');
   
   // imageBlock.set({
   //   label: 'Updated block',
   // });
-  
 
-  bm.add('image2', {
-    // category,
-    // select: true,
-    // activate: true,
-    label: "IMAGE2",
-    media: `<svg viewBox="0 0 24 24">
-      <path fill="currentColor" d="M21,3H3C2,3 1,4 1,5V19A2,2 0 0,0 3,21H21C22,21 23,20 23,19V5C23,4 22,3 21,3M5,17L8.5,12.5L11,15.5L14.5,11L19,17H5Z" />
-    </svg>`,
-    content: {
-      style: { color: 'black' },
-      type: 'image',
-    }
-  });
-
+  // bm.add('image2', {
+  //   // category,
+  //   // select: true,
+  //   // activate: true,
+  //   label: "IMAGE2",
+  //   media: `<svg viewBox="0 0 24 24">
+  //     <path fill="currentColor" d="M21,3H3C2,3 1,4 1,5V19A2,2 0 0,0 3,21H21C22,21 23,20 23,19V5C23,4 22,3 21,3M5,17L8.5,12.5L11,15.5L14.5,11L19,17H5Z" />
+  //   </svg>`,
+  //   content: {
+  //     style: { color: 'black' },
+  //     type: 'image',
+  //   }
+  // });
 
   editor.render();
 });
