38d33cab8bac39098a9a889a258642b622d39554
Stefan Bohacek <stefan@stefanbohacek.com>
2025-06-01 17:47:59 -0400

    Migrated SB.dev to SB.com.

--- src/scripts/modules/matomo.js
+++ src/scripts/modules/matomo.js
@@ -1,7 +1,7 @@
 export default () => {
   setTimeout(() => {
     if (!window.Matomo) {
-      const url = `https://tools.stefanbohacek.dev/info?idsite=1&action_name=${encodeURIComponent(document.title)}&url=${window.location.href}&uadata=${encodeURIComponent(JSON.stringify(navigator.userAgentData))}&res=${window.screen.availWidth}x${window.screen.availHeight}`;
+      const url = `https://tools.stefanbohacek.com/info?idsite=1&action_name=${encodeURIComponent(document.title)}&url=${window.location.href}&uadata=${encodeURIComponent(JSON.stringify(navigator.userAgentData))}&res=${window.screen.availWidth}x${window.screen.availHeight}`;
       fetch(url);
     }
   }, 2000);
